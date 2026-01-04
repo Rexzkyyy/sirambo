82e77bb90264bfd769a7523bbbd43211bd892530
Rexzkyyy <90616248+Rexzkyyy@users.noreply.github.com>
2026-01-04 23:47:59 +0800

    Revamp UI with Bootstrap 5 layout

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -10,8 +10,8 @@ $base = rtrim(BASE_URL, '/');
 
   <!-- Bootstrap 5 CDN -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
   <link href="<?= $base ?>/assets/css/app.css" rel="stylesheet">
 </head>
-<body class="bg-light">
-<div class="container-fluid">
-  <div class="row">
+<body class="app-body bg-light">
+  <div class="app-shell d-flex flex-column min-vh-100">
